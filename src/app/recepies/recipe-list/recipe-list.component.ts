d6c361214a0c7a701ada0e851a6ab0945b956a7d
Naveen Kumar K G <navinkumar618@gmail.com>
2019-07-05 14:07:35 +0530

    adding recipe content

--- src/app/recepies/recipe-list/recipe-list.component.ts
+++ src/app/recepies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes: Recipe[] = [
+  // tslint:disable-next-line:max-line-length
+  new Recipe('Test recipe', 'Test recipe descitption', 'https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+];
   constructor() { }
 
   ngOnInit() {
